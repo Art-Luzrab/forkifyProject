78a37182b6ee70409470c6498c8c8e043ef01d1b
Arthur <arthur91021@gmail.com>
2024-06-05 10:42:44 -0700

    added search results loadability

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,9 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime';
-
-// const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
 
@@ -27,8 +24,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    //3 Render results
+    console.log(model.state.search.results, model.state.search.query);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 // window event listener that excecutes controlRecipes
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
