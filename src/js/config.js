f4e67a580c8a352544ee4fd1e449a6ab4c66b6b0
Arthur <arthur91021@gmail.com>
2024-07-01 15:21:36 -0700

    uploaded data successfully sends it to forkifyapi

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'f5e771bd-c3dc-4b52-981c-d899b42164aa';
+export const MODAL_CLOSE_SEC = 2.5;
